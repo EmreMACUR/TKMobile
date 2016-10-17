a75db1536cd38fa39dbefaacd35bd8b23bfc34db
oguzhancolak <oguzhan.colak@in2project.com>
2016-10-17 11:08:32 +0300

    sqlite bug fixed

--- app/scripts/controllers/DistributionController.js
+++ app/scripts/controllers/DistributionController.js
@@ -1,7 +1,7 @@
 /**
  * Created by oguzhancolak on 7.10.2016.
  */
-app.controller('DistributionController', function($scope, Chats, SyncSrvc, $rootScope) {
+app.controller('DistributionController', function($scope, Chats) {
   $scope.data = {
     showDelete: false
   };
@@ -15,21 +15,5 @@ app.controller('DistributionController', function($scope, Chats, SyncSrvc, $root
     $scope.chats.splice(fromIndex, 1);
     $scope.chats.splice(toIndex, 0, item);
   };
-
-  SyncSrvc.setDatabase();
-  SyncSrvc.sync();
-  SyncSrvc.startListening();
-
-  $scope.items = {};
-
-  $rootScope.$on("$pouchDB:change", function(event, data) {
-    $scope.items[data.doc._id] = data.doc;
-    $scope.$apply();
-  });
-
-  $rootScope.$on("$pouchDB:delete", function(event, data) {
-    delete $scope.items[data.doc._id];
-    $scope.$apply();
-  });
 
 });
--- app/scripts/controllers/SettingsController.js
+++ app/scripts/controllers/SettingsController.js
@@ -1,7 +1,7 @@
 /**
  * Created by oguzhancolak on 7.10.2016.
  */
-app.controller('SettingsController', function($scope) {
+app.controller('SettingsController', function($scope, SyncSrvc, $rootScope) {
   $scope.settings = {
     enableWifi: true
   };
@@ -10,4 +10,20 @@ app.controller('SettingsController', function($scope) {
     var number = '05074290210' ;
     window.location.href = 'tel:'+ number;
   };
+
+  SyncSrvc.setDatabase();
+  SyncSrvc.sync();
+  SyncSrvc.startListening();
+
+  $scope.items = {};
+
+  $rootScope.$on("$pouchDB:change", function(event, data) {
+    $scope.items[data.doc._id] = data.doc;
+    $scope.$apply();
+  });
+
+  $rootScope.$on("$pouchDB:delete", function(event, data) {
+    delete $scope.items[data.doc._id];
+    $scope.$apply();
+  });
 });
